6925911a3bb2c77788fb88966531d7eb41142fe3
SSR <iamssrobinson@gmail.com>
2023-07-06 17:16:23 +0100

    Coding with intent.

--- 01_javascript_collections/08_pedac_guided_practice_sort_by_most_adjacent_consonants/06_code_with_intent_sortByMostAdjacentConsonants.js
+++ 01_javascript_collections/08_pedac_guided_practice_sort_by_most_adjacent_consonants/06_code_with_intent_sortByMostAdjacentConsonants.js
@@ -1,28 +1,28 @@
 // #{target-code}
 
-function consonantCounts (string) {
+function consonantCounts(string) {
   let counts = [];
   let count = 0;
   let obj = {};
 
-  string = string.includes(' ') ? removeStringSpace(string) : string;
+  string = string.includes(" ") ? removeStringSpace(string) : string;
 
   for (let index = 0; index <= string.length; index++) {
-   if (isValidConsonant(string[index])) {
-    count++;
-   } else {
-    switch (count) {
-      case 0:
-      case 1:
-        counts.push(0);
-        count = 0;
-        break;
-      default:
-        counts.push(count);
-        count = 0;
-        break;
+    if (isValidConsonant(string[index])) {
+      count++;
+    } else {
+      switch (count) {
+        case 0:
+        case 1:
+          counts.push(0);
+          count = 0;
+          break;
+        default:
+          counts.push(count);
+          count = 0;
+          break;
+      }
     }
-   }
   }
 
   count = counts.reduce((acc, el) => Math.max(acc, el));
@@ -31,23 +31,26 @@ function consonantCounts (string) {
   return obj;
 }
 
-function removeStringSpace (string){
-  let stringSpaceRemoved = string.split('').filter(char => char !== ' ').join('');
-  
+function removeStringSpace(string) {
+  let stringSpaceRemoved = string
+    .split("")
+    .filter((char) => char !== " ")
+    .join("");
+
   return stringSpaceRemoved;
 }
 
-function isValidConsonant (char) {
-  let validConsonants = 'bcdfghjklmnpqrstvwxyz';
+function isValidConsonant(char) {
+  let validConsonants = "bcdfghjklmnpqrstvwxyz";
   return validConsonants.includes(char);
 }
 
 // TEST EXAMPLES: INDIVIDUAL STRINGS:
-console.log(consonantCounts('aa')) // { aa: 0 }
-console.log(consonantCounts('baa')) // { baa: 0 }
-console.log(consonantCounts('baabb')) // { baabb: 2 }
-console.log(consonantCounts('dddaa')) // { dddaa: 3 }
-console.log(consonantCounts('salt pan')) // { saltpan: 3 }
+console.log(consonantCounts("aa")); // { aa: 0 }
+console.log(consonantCounts("baa")); // { baa: 0 }
+console.log(consonantCounts("baabb")); // { baabb: 2 }
+console.log(consonantCounts("dddaa")); // { dddaa: 3 }
+console.log(consonantCounts("salt pan")); // { saltpan: 3 }
 
 // NEXT: TEST ARRAY OF STRINGS:
 // ['aa', 'baa', 'ccaa', 'dddaa', 'salt pan']
