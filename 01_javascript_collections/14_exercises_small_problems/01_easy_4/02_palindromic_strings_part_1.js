0b2ee3fd2f1efe6bb514a7d95a77fa820ae921ce
SSR <iamssrobinson@gmail.com>
2023-07-10 12:06:10 +0100

    Understanding method chaining and use of the methods: .split() .reverse() and .join().

--- 01_javascript_collections/14_exercises_small_problems/01_easy_4/02_palindromic_strings_part_1.js
+++ 01_javascript_collections/14_exercises_small_problems/01_easy_4/02_palindromic_strings_part_1.js
@@ -1,17 +1,19 @@
-// Palindromic Strings (Part 1)
-// Write a function that returns true if the string passed as an argument is a palindrome, or false otherwise. A palindrome reads the same forwards and backwards. For this problem, the case matters and all characters matter.
-
-// Examples:
-
-// Copy Code
-console.log(isPalindrome('madam'));               // true
-console.log(isPalindrome('Madam'));               // false (case matters)
-console.log(isPalindrome("madam i'm adam"));      // false (all characters matter)
-console.log(isPalindrome('356653'));              // true
-
 function isPalindrome (string) {
 
   let reversed = string.split('').reverse().join('');
 
   return string === reversed;
 }
+
+console.log(isPalindrome('madam'));               // true
+console.log(isPalindrome('Madam'));               // false (case matters)
+console.log(isPalindrome("madam i'm adam"));      // false (all characters matter)
+console.log(isPalindrome('356653'));              // true
+
+
+/*
+
+Palindromic Strings (Part 1)
+Write a function that returns true if the string passed as an argument is a palindrome, or false otherwise. A palindrome reads the same forwards and backwards. For this problem, the case matters and all characters matter.
+
+*/
