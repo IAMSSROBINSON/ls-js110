1e44d3db4de7b2016e24b8ad108bd31ff23ddda8
SSR <iamssrobinson@gmail.com>
2023-07-15 23:32:30 +0100

    Understanding string manipulation.

---
+++ 01_javascript_collections/14_exercises_small_problems/01_easy_4/08_letter_swap.js
@@ -0,0 +1,26 @@
+function swap (stringOfWords) {
+let splitStrings = stringOfWords.split(' ');
+splitStrings.forEach((word, index)=>{
+  let firstLetter = word[0];
+  let lastLetter = word[word.length -1];
+  if (word.length > 1) {
+    splitStrings[index] = lastLetter + word.slice(1, word.length - 1) + firstLetter;
+  }
+ 
+})
+return splitStrings.join(', ')
+}
+
+
+
+console.log(swap('Oh what a wonderful day it is'));  // "hO thaw a londerfuw yad ti si"
+console.log(swap('Abcde'));                          // "ebcdA"
+console.log(swap('a'));                              // "a"
+
+/*
+
+Given a string of words separated by spaces, write a function that swaps the first and last letters of every word.
+
+You may assume that every word contains at least one letter, and that the string will always contain at least one word. You may also assume that each string contains nothing but words and spaces, and that there are no leading, trailing, or repeated spaces.
+
+*/
