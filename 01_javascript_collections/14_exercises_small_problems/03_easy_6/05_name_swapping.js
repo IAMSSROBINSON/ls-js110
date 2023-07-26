318962bc9b5a2f887bbaa29546cd9a34554734bd
SSR <iamssrobinson@gmail.com>
2023-07-26 17:33:23 +0100

    Added instruction to explore problem further.

--- 01_javascript_collections/14_exercises_small_problems/03_easy_6/05_name_swapping.js
+++ 01_javascript_collections/14_exercises_small_problems/03_easy_6/05_name_swapping.js
@@ -8,4 +8,7 @@ console.log(swapName('Joe Roberts'));    // "Roberts, Joe"
 
 Write a function that takes a string argument consisting of a first name, a space, and a last name, and returns a new string consisting of the last name, a comma, a space, and the first name.
 
+Further Exploration
+What if the person has one or more middle names? Refactor the current solution so that it can accommodate this; the middle names should be listed after the first name:
+
 */
