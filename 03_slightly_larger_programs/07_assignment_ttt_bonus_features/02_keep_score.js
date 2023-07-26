4bbc9753a61bbbb75141ebb80f51beb37ea43223
SSR <iamssrobinson@gmail.com>
2023-07-26 10:51:00 +0100

    Started game implementation again.

--- 03_slightly_larger_programs/07_assignment_ttt_bonus_features/02_keep_score.js
+++ 03_slightly_larger_programs/07_assignment_ttt_bonus_features/02_keep_score.js
@@ -1,1 +1,26 @@
-d
+const READLINE = require('readline-sync');
+const MESSAGES = require('./02_keep_score.messages.json')
+const BOARD_SIZE = 9;
+const SQUARE_PLACEHOLDER = ' ';
+
+function prompt (message) {
+  console.log(`=> ${message}`);
+}
+
+function getBoard () {
+  let board = {};
+
+  for (let square = 1; square <= BOARD_SIZE; square++) {
+    board[square] = SQUARE_PLACEHOLDER;
+  }
+  return board;
+}
+
+
+// MAIN PROGRAM
+prompt(MESSAGES.welcome + '\n');
+prompt(MESSAGES.select);
+
+let board = getBoard();
+console.log(board);
+
