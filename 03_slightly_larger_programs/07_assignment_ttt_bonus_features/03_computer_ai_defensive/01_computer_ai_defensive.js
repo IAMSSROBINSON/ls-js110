b088c92b6ba6418c24b8e759878c3c04d0b4e564
SSR <iamssrobinson@gmail.com>
2023-07-29 21:35:15 +0100

    Computer now selects a threat square if one is availableor a random square.

---
+++ 03_slightly_larger_programs/07_assignment_ttt_bonus_features/03_computer_ai_defensive/01_computer_ai_defensive.js
@@ -0,0 +1,265 @@
+const READLINE = require("readline-sync");
+const MESSAGES = require("./01_computer_ai_defensive.json");
+const BOARD_SIZE = 9;
+const SQUARE_PLACEHOLDER = " ";
+const USER_MARKER = "X";
+const COMPUTER_MARKER = "O";
+const GRAND_WINNER = 5;
+const WINNING_COMBOS = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [1, 4, 7],
+  [2, 5, 8],
+  [3, 6, 9],
+  [1, 5, 9],
+  [3, 5, 7],
+];
+
+function prompt(message) {
+  console.log(`=> ${message}`);
+}
+
+function getBoard() {
+  let board = {};
+
+  for (let square = 1; square <= BOARD_SIZE; square++) {
+    board[String(square)] = SQUARE_PLACEHOLDER;
+  }
+
+  return board;
+}
+
+function displayBoard(board) {
+  console.clear();
+  prompt(`${MESSAGES.displayUserMarker} ${USER_MARKER}`);
+  prompt(`${MESSAGES.displayComputerMarker} ${COMPUTER_MARKER}`);
+
+  console.log(``);
+  console.log(`     |     |     `);
+  console.log(`  ${board["1"]}  |  ${board["2"]}  |  ${board["3"]}  `);
+  console.log(`     |     |     `);
+  console.log(`-----+-----+-----`);
+  console.log(`     |     |     `);
+  console.log(`  ${board["4"]}  |  ${board["5"]}  |  ${board["6"]}  `);
+  console.log(`     |     |     `);
+  console.log(`-----+-----+-----`);
+  console.log(`     |     |     `);
+  console.log(`  ${board["7"]}  |  ${board["8"]}  |  ${board["9"]}  `);
+  console.log(`     |     |     `);
+  console.log(``);
+}
+
+function getAvailableSquares(board) {
+  return Object.keys(board).filter((squares) => board[squares] === " ");
+}
+
+function getUserMove(board) {
+  let userMove = READLINE.question().trim();
+
+  while (!getAvailableSquares(board).includes(String(userMove))) {
+    prompt(
+      `${MESSAGES.promptInvalidSquare} ${joinerOr(
+        getAvailableSquares(board),
+        "; ",
+        " and "
+      )}`
+    );
+    userMove = READLINE.question();
+  }
+
+  board[userMove] = USER_MARKER;
+}
+
+function getComputerMove(board) {
+  let computerMove;
+
+  if (isThreat(board)) {
+    computerMove = getThreat(board);
+  } else {
+    let randomNumber = Math.floor(
+      Math.random() * getAvailableSquares(board).length
+    );
+    computerMove = getAvailableSquares(board)[randomNumber];
+  }
+  board[computerMove] = COMPUTER_MARKER;
+}
+
+function getThreat(board) {
+  let computerMove;
+  let availableSquares = getAvailableSquares(board);
+
+  let threatArray = isThreat(board);
+
+  for (let index = 0; index < availableSquares.length; index++) {
+    let currentAvailableSquare = availableSquares[index];
+
+    for (let idx = 0; idx < threatArray.length; idx++) {
+      if (currentAvailableSquare === String(threatArray[idx])) {
+        computerMove = currentAvailableSquare;
+        break;
+      }
+    }
+  }
+  return computerMove;
+}
+
+function isThreat(board) {
+  return WINNING_COMBOS.find((nestedArray) => {
+    return (
+      (board[nestedArray[0]] === USER_MARKER &&
+        board[nestedArray[1]] === USER_MARKER &&
+        board[nestedArray[2]] === " ") ||
+      (board[nestedArray[1]] === USER_MARKER &&
+        board[nestedArray[2]] === USER_MARKER &&
+        board[nestedArray[0]] === " ") ||
+      (board[nestedArray[0]] === USER_MARKER &&
+        board[nestedArray[2]] === USER_MARKER &&
+        board[nestedArray[1]] === " ")
+    );
+  });
+}
+
+function joinerOr(array, delimiter = ", ", word = "or") {
+  if (array.length <= 1) {
+    return array;
+  }
+  return (
+    array.slice(0, array.length - 1).join(`${delimiter}`) +
+    word +
+    array.slice(array.length - 1)
+  );
+}
+
+function isWinner(board) {
+  return (
+    WINNING_COMBOS.some((arrayOfCombos) =>
+      arrayOfCombos.every((element) => board[element] === USER_MARKER)
+    ) ||
+    WINNING_COMBOS.some((arrayOfCombos) =>
+      arrayOfCombos.every((element) => board[element] === COMPUTER_MARKER)
+    )
+  );
+}
+
+function isNoSquaresLeft(board) {
+  return getAvailableSquares(board).length === 0;
+}
+
+function getWinner(board) {
+  let winner;
+  if (
+    WINNING_COMBOS.some((arrayOfCombos) =>
+      arrayOfCombos.every((element) => board[element] === USER_MARKER)
+    )
+  ) {
+    winner = MESSAGES.displayUserName;
+  } else {
+    winner = MESSAGES.displayComputerName;
+  }
+
+  return winner;
+}
+
+function scoreManager() {
+  let score = 0;
+
+  function getScore() {
+    return score;
+  }
+
+  function increaseScore() {
+    score += 1;
+  }
+
+  function resetScore() {
+    score = 0;
+  }
+
+  return { getScore, increaseScore, resetScore };
+}
+
+function incrementWinnerScore(userOrComputer, userPlayer, computerPlayer) {
+  if (userOrComputer === "User") {
+    userPlayer.increaseScore();
+  } else {
+    computerPlayer.increaseScore();
+  }
+}
+
+function displayScores(userPlayer, computerPlayer) {
+  console.log("");
+  prompt(`${MESSAGES.displayScoresHeader}`);
+  prompt(`${MESSAGES.displayUserName}: ${userPlayer.getScore()}`);
+  prompt(`${MESSAGES.displayComputerName}: ${computerPlayer.getScore()}`);
+  console.log("");
+}
+
+function isGrandWinner(userPlayer, computerPlayer) {
+  return (
+    userPlayer.getScore() === GRAND_WINNER ||
+    computerPlayer.getScore() === GRAND_WINNER
+  );
+}
+
+function displayGrandWinner(winner, userPlayer, computerPlayer) {
+  prompt(`${winner}${MESSAGES.displayGrandWinner}`);
+
+  userPlayer.resetScore();
+  computerPlayer.resetScore();
+}
+
+// MAIN PROGRAM
+prompt(MESSAGES.displayWelcome);
+prompt(MESSAGES.displayRules1);
+prompt(MESSAGES.displayRules2);
+prompt(MESSAGES.displayRules3);
+prompt(MESSAGES.displayReadToPlay + "\n");
+setTimeout(startProgram, 8000);
+
+function startProgram() {
+
+  let userPlayer = scoreManager();
+  let computerPlayer = scoreManager();
+
+  while (true) {
+    let board = getBoard();
+    displayBoard(board);
+
+    while (true) {
+      prompt(
+        `${MESSAGES.promptSelect} ${joinerOr(
+          getAvailableSquares(board),
+          ", ",
+          " or "
+        )}`
+      );
+
+      getUserMove(board);
+      displayBoard(board);
+      if (isNoSquaresLeft(board) || isWinner(board)) break;
+
+      getComputerMove(board);
+      displayBoard(board);
+      if (isNoSquaresLeft(board) || isWinner(board)) break;
+    }
+
+    if (isWinner(board)) {
+      prompt(`${MESSAGES.displayWinnerIs} ${getWinner(board)}`);
+      incrementWinnerScore(getWinner(board), userPlayer, computerPlayer);
+    } else {
+      prompt(MESSAGES.displayTiedGame);
+    }
+    displayScores(userPlayer, computerPlayer);
+    if (isGrandWinner(userPlayer, computerPlayer)) {
+      displayGrandWinner(getWinner(board), userPlayer, computerPlayer);
+    }
+
+    prompt(MESSAGES.promptPlayAgain);
+    let playAgain = READLINE.question();
+    if (playAgain[0].toLowerCase() !== "y") {
+      prompt(MESSAGES.displayGoodbye);
+      break;
+    }
+  }
+};
---
+++ 03_slightly_larger_programs/07_assignment_ttt_bonus_features/03_computer_ai_defensive/01_computer_ai_defensive.md
@@ -0,0 +1,126 @@
+# Computer AI: Defensive
+
+The computer currently picks a square at random. That's not very interesting. Let's make the computer defensive-minded so that, when an immediate threat exists, it will try to defend the 3rd square. An immediate threat occurs when the human player has 2 squares in a row with the 3rd square unoccupied. If there's no immediate threat, the computer can pick a random square.
+
+<br>
+
+## P: Understanding The Problem
+
+Create a function
+When it is the computers turn to place a marker
+First check if the User has occupied (with their marker) 2 out of 3 squares in the winning_combos array of nested winner combo arrays.
+If the user has occupied 2 squares with the potential for a win on the 3rd then return that winning combo array and have the computer select the unoccupied square.
+
+Input: board(object of occupied squares(with markers) and unoccupied squares thus far in the game)
+Output: array combo where 2 squares are occupied(===) with the USER_MARKER 'X'
+Rules:
+  Explicit:
+  - must identify 2 occupied squares from at least 1 array in the WINNING_COMBOS arrays
+    - this will leave 1 square === ' '; 
+    - the computer should select this square for its move to save the same
+
+
+  Implicit:
+  - n/a
+
+Questions:
+  - there may be more than 1 array identified with 2 occupied squares, what will you do?
+    - return the first array  
+
+<br>
+
+## E: Examples and Test Cases
+```JavaScript
+const WINNING_COMBOS = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [1, 4, 7],
+  [2, 5, 8],
+  [3, 6, 9],
+  [1, 5, 9],
+  [3, 5, 7],
+];
+
+board:
+     |     |     
+  X  |  X  |     
+     |     |     
+-----+-----+-----
+     |     |     
+  X  |     |  O  
+     |     |     
+-----+-----+-----
+     |     |     
+  O  |     |    
+     |     |     
+
+board object:
+{
+  '1': 'X',
+  '2': 'X',
+  '3': ' ',
+  '4': 'X',
+  '5': ' ',
+  '6': 'O',
+  '7': 'O',
+  '8': ' ',
+  '9': ' '
+}
+```
+
+Mental Model:
+From the board object we can see the occupied and unoccupied squares.
+When comparing the board object keys occupied and the USER_MARKER value('X') with the WINNING_COMBOS array we see that one of the nested arrays: [1, 2, 3] has 2/3 elements of the array === USER_MARKER with one element unoccupied:
+
+WINNING_COMBOS: [[1, 2, 3]] 
+
+Occupied:
+board['1'] === 'X'
+board['2'] === 'X'
+board['4'] === 'X'
+
+Unoccupied threat:
+board['3'] === ' '
+
+<br>
+
+## D: Data Structure
+Array: I will potentially utilize an array structure to filter out required values.
+
+A: Algorithm
+
+Informal:
+- Before the computer randomly selects an available square
+
+- Create a function that finds out if there is a threat square(a square that will enable the User to win by completing a WINNING_COMBO)
+  
+- get the first array that is a threat:
+- array.find(theFirstNestedArrayAsThereMayBeMoreThanOne) in the WINNING_COMBOS array:
+  that meets one of these tests:
+  - iterate over the winningCombos array(with nested arrays)
+  - test to make sure 2 out of 3 squares are occupied by the userMarker and 1 square is available
+    - For each nested array inside of the winningCombos array:
+    - return the first array that matches these conditions and save to variable threatArray:
+    (board[nestedArray[0]] === USER_MARKER &&
+        board[nestedArray[1]] === USER_MARKER) && board[nestedArray[2]] === ' ' ||
+      (board[nestedArray[1]] === USER_MARKER &&
+        board[nestedArray[2]] === USER_MARKER) && board[nestedArray[0]] === ' ' ||
+      (board[nestedArray[0]] === USER_MARKER &&
+        board[nestedArray[2]] === USER_MARKER && board[nestedArray[1]] === ' ')
+  - e.g. let threatArray = [ 1, 4, 7 ]
+  
+- use the availableSquares function to return a list of the available squares 
+  - e.g. [2, 7, 9]
+
+- iterate over the available squares and set a currentSquare variable
+  - nest a loop and iterate over the threatArray
+    - if currentSquare matches one of the values in the threatArray then set to computers move
+
+Formal:
+Since this will require some manipulation to integrate into the flow of the program as it stands a formal  rendition of the algorithm will not be useful since some juggling will occur.
+
+<br>
+
+## C: Code With Intent
+[link to code](./01_computer_ai_defensive.js)
