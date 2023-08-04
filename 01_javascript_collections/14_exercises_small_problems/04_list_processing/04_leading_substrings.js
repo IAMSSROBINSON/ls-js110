07856b7852ae26a5066970210d91fa63855f9bc2
SSR <iamssrobinson@gmail.com>
2023-08-04 18:47:08 +0100

    Added instruction for further exploration.

--- 01_javascript_collections/14_exercises_small_problems/04_list_processing/04_leading_substrings.js
+++ 01_javascript_collections/14_exercises_small_problems/04_list_processing/04_leading_substrings.js
@@ -58,4 +58,8 @@ A: Algorithm
 
 C: Code With Intent
 
+
+Further Exploration
+Rewrite leadingSubstrings using list processing functions. That is, convert the string into an array of some sort and use functions like map, filter, or reduce to get the desired substrings. (You will also need to use join.) Try not to use forEach as that is too similar to the for loop approach.
+
 */
