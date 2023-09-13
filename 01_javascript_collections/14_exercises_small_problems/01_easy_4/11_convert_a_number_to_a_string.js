0db1e47a381131b6e7d6dd0dae7ca4c7aa5849a5
SSR <iamssrobinson@gmail.com>
2023-09-13 19:32:26 +0100

    Understanding number to string conversion.

--- 01_javascript_collections/14_exercises_small_problems/01_easy_4/11_convert_a_number_to_a_string.js
+++ 01_javascript_collections/14_exercises_small_problems/01_easy_4/11_convert_a_number_to_a_string.js
@@ -1,7 +1,21 @@
-integerToString(4321);        // "4321"
-integerToString(0);           // "0"
-integerToString(5000);        // "5000"
-integerToString(1234567890);  // "1234567890"
+function integerToString (number) {
+  if (number === 0) {
+    return '0';
+  }
+
+  let finalString = '';
+  while (number > 0) {
+    let digit = number % 10;
+    finalString = digit + finalString;
+    number = Math.floor(number / 10);
+  }
+  return finalString;
+}
+
+console.log(integerToString(4321));        // "4321"
+console.log(integerToString(0));           // "0"
+console.log(integerToString(5000));        // "5000"
+console.log(integerToString(1234567890));  // "1234567890"
 
 /*
 
@@ -9,59 +23,59 @@ Write a function that converts a non-negative integer value (e.g., 0, 1, 2, 3, a
 
 You may not use any of the standard conversion functions available in JavaScript, such as String(), Number.prototype.toString, or an expression such as '' + number. Your function should do this the old-fashioned way and construct the string by analyzing and manipulating the number.
 
-P: UNDERSTAND THE PROBLEM
-  - Clarification:
 
-  Create a function that takes a positive number and converts it to a string without using any explicit conversion functions or methods.
+P: Understanding The Problem
+Create a function that takes a number.
+The function should convert the number into a stringOfDigits.
+Method should not be use but instead manipulate the number.
 
-  INPUT: int (positive numbers)
-  OUTPUT: string (string data type of the same digits)
+Input: Number(positive number)
+Output: String(representation of the numbers)
+Rules:
+  Explicit:
+  - Create a function
+  - Receive the number argument
+  - Convert the number to a stringOfDigits
+  - Do not use methods
+  - Do manipulate the number
 
-  RULES & REQUIREMENTS:
-    EXPLICIT :
-      - no methods
-      - no built in functions/methods
-        - toString X
-        - String X
-      - no implicit coercion from expression conversion using '' X
-      - no concatenation operator + implicit conversion X
-      - analyze and manipulate each number
+  Implicit:
+  - number could be 0 returning '0'
 
-    IMPLICIT:
-      - function must perform the conversion
-      - return value must be a primitive string value
-      - return value must evaluate to true for non-strict / loose equality ==
+Questions:
+- n/a
 
-    QUESTIONS:
-    - do I need to separate the number into a single digit and work on the number on a digit by digit basis?
-      - yes
-      (how will you do this)
 
-    - how would you covert a number to a string without functions/methods?
-      - you would need some kind of a lookup table with string char counterparts
+E: Examples and Test Cases
+Input: 1234567890
+Output: "1234567890"
 
-    - what functions/methods should you not use?
-      - see explicit requirements above
+Mental Model:
+% the number by 10 to get the last digit: 0
+concatenate the digit to an empty string "" to  => 0 + '' = '0'
+reassign the number to the Math.floor(number / 10) to round the number and remove the remainder
+loop and repeat
 
-    
 
-E: EXAMPLES AND TEST CASES
-  4321 => '4321' 
-  0 => '0' 
-  5000 => '5000' 
-  1234567890 => '1234567890' 
+D: Data Structure
+I will utilize variable storage to assign and reassign values as required since no array or object structures suit the requirements.
 
-  typeof '4321' === string => true
 
-  MENTAL MODEL FOR SOLVING:
-  
-  
+A: Algorithm
+- start
+- set function
+- receive number argument
+- set guard clause for 0 to return '0';
+- set finalString variable = ''
+- loop
+  - while the number > 0
+  - set digit variable: number % 10;
+  - reassign finalString = digit + finalString so that the char appear from the outside inward
+  - reassign number to be Math.floor(number / 10)
+- return the finalString
+- end
 
-D: DATA STRUCTURE
- 
 
-A: ALGORITHM
-  
-C: CODE
+C: Code With Intent
 
 */
